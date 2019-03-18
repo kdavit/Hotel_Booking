1cb533f3237b39cb1b0dc857cc94823b2062b0a3
kdavit <davitkurcxalia7@gmail.com>
2019-03-19 01:32:52 +0400

    updated main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import Models.Hotel;
 import Models.Room;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -13,24 +10,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-//        ArrayList<Hotel> hotelTest = new ArrayList<>();
-//        ArrayList<Room> roomTest = new ArrayList<>();
-//
-//
-//        Filter test = new Filter(hotelTest);
-//
-//        for (Hotel hotel : test.filterHotels("tbiliso", "saqartvelo", "tbilisi", 4)) {
-//            System.out.println("Name: " + hotel.getName());
-//            System.out.println("Attached rooms:");
-//            for (Room room : hotel.getRooms()) {
-//                System.out.println(room.getNumber());
-//            }
-//            System.out.println("-------------------");
-//        }
 
         Filter filter = new Filter();
-        filter.filterHotels("საქართველო","ბათუმი",4);
-        System.out.println(filter);
+        List<Hotel> test =  filter.filterHotels("საქართველო","ბათუმი",4);
+        List<Room> testroom = filter.filterRooms(test,115);
+        for (Hotel hotel:test)
+        {
+            System.out.printf(hotel.getName());
+            for (Room room : testroom)
+            {
+                System.out.println("  " + room.getNumber());
+            }
+        }
+
+
 
         List<Hotel> l = DataLoader.listLoader();
 
