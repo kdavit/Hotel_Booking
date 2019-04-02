dcdb277986090ab578f1d095a9cafe6c6125ce3a
kdavit <davitkurcxalia7@gmail.com>
2019-04-02 19:38:57 +0400

    updated MyGUIForm

--- src/gui/MyGUIForm.java
+++ src/gui/MyGUIForm.java
@@ -28,28 +28,29 @@ public class MyGUIForm extends JFrame {
             String star = starField.getText();
             String price = priceField.getText();
 
-            if(country.isEmpty() || city.isEmpty() || star.isEmpty() || price.isEmpty()) {
+            if (country.isEmpty() || city.isEmpty() || star.isEmpty() || price.isEmpty()) {
                 JOptionPane.showMessageDialog(rootPanel, "Please enter text");
             } else {
                 Filter filter = new Filter();
                 List<Hotel> test = filter.filterHotels(country, city, Integer.parseInt(star), Integer.parseInt(price));
-                StringBuilder x = new StringBuilder();
-                for (Hotel hotel : test) {
-                    x.append("სასტუმრო: ").append(hotel.getName()).append("\nოთახები: ");
-                    for (int i = 0; i < hotel.getRooms().size(); i++) {
-                        x.append("\n").append(hotel.getRooms().get(i).getNumber()).append(";");
-                    }
-                    x.append("\n");
-                }
-                if(x.toString().isEmpty()) {
+                if (test.isEmpty()) {
                     JOptionPane.showMessageDialog(rootPanel, "Can't find matching hotels");
                 } else {
+                    StringBuilder x = new StringBuilder();
+                    for (Hotel hotel : test) {
+                        x.append("სასტუმრო: ").append(hotel.getName()).append("\nოთახები: ");
+                        for (int i = 0; i < hotel.getRooms().size(); i++) {
+                            x.append("\n").append(hotel.getRooms().get(i).getNumber()).append(";");
+                        }
+                        x.append("\n");
+                    }
                     FilteredGUI filteredGUI = new FilteredGUI();
                     filteredGUI.setHotelsTextArea(x.toString());
                     filteredGUI.setVisible(true);
                     filteredGUI.pack();
                 }
             }
+
 
         });
     }
